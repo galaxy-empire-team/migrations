488db2826ff091bd521a6e20e872101c194dd46a
bormonoff <bormon_off@protonmail.com>
2026-05-09 06:16:53 +0300

    Add version field to user_notifications table
    Version field describes the schema version of the data (jsonb) column
    in user_notifications table
    
    Changes:
    - (user_notifications): Add version column
    - (planet_resources): Add constraint for positive resource values in planet_resources table

--- migrations/20260117100955_create_planet_resources_table.sql
+++ migrations/20260117100955_create_planet_resources_table.sql
@@ -3,9 +3,9 @@
 CREATE TABLE IF NOT EXISTS session_beta.planet_resources (
     id serial PRIMARY KEY,
     planet_id uuid UNIQUE NOT NULL,
-    metal bigint NOT NULL,
-    crystal bigint NOT NULL,
-    gas bigint NOT NULL,
+    metal bigint NOT NULL CHECK (metal >= 0),
+    crystal bigint NOT NULL CHECK (crystal >= 0),
+    gas bigint NOT NULL CHECK (gas >= 0),
     updated_at timestamptz NOT NULL DEFAULT NOW(),
 
     FOREIGN KEY (planet_id) REFERENCES session_beta.planets(id) ON DELETE RESTRICT
--- migrations/20260221062749_create_user_notifications_table.sql
+++ migrations/20260221062749_create_user_notifications_table.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS session_beta.user_notifications (
     id serial PRIMARY KEY,
     user_id uuid NOT NULL,
     notification_id integer NOT NULL,
+    version integer NOT NULL,
     data jsonb,
     is_read boolean NOT NULL DEFAULT false,
     created_at timestamptz NOT NULL DEFAULT NOW(),
